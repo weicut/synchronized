9cd4fa38b326b81ab86320315187f690648b4fc2
fangjw <fangjw@wecut.com>
2021-06-09 12:09:21 +0800

    Added tests

--- tests/BlockLockTest.php
+++ tests/BlockLockTest.php
@@ -5,9 +5,37 @@ declare(strict_types=1);
 namespace HyperfTest\Synchronized;
 
 
+use Hyperf\Synchronized\Annotation\Synchronized;
+use Hyperf\Synchronized\LockMode;
+use Hyperf\Utils\Parallel;
 use PHPUnit\Framework\TestCase;
 
 class BlockLockTest extends TestCase
 {
 
+    public function testLock(){
+
+        $startTime = microtime(true);
+
+        $parallel = new Parallel(5);
+
+        $parallel->add([$this, 'handler']);
+        $parallel->add([$this, 'handler']);
+        $parallel->add([$this, 'handler']);
+        $parallel->add([$this, 'handler']);
+        $parallel->add([$this, 'handler']);
+
+        $parallel->wait();
+
+        $endTime = microtime(true);
+
+        self::assertGreaterThan($endTime - $startTime, 5);
+    }
+
+    /**
+     * @Synchronized(mode=LockMode::BLOCK, secondsTimeout=3)
+     */
+    public function handler():void{
+        sleep(1);
+    }
 }
--- tests/ExceptionLockTest.php
+++ tests/ExceptionLockTest.php
@@ -5,9 +5,37 @@ declare(strict_types=1);
 namespace HyperfTest\Synchronized;
 
 
+use Hyperf\Synchronized\Annotation\Synchronized;
+use Hyperf\Synchronized\LockMode;
+use Hyperf\Utils\Parallel;
 use PHPUnit\Framework\TestCase;
 
 class ExceptionLockTest extends TestCase
 {
+    public function testLock(){
 
+        $parallel = new Parallel(5);
+
+        $parallel->add([$this, 'handler']);
+        $parallel->add([$this, 'handler']);
+        $parallel->add([$this, 'handler']);
+        $parallel->add([$this, 'handler']);
+        $parallel->add([$this, 'handler']);
+
+        $e = null;
+        try{
+            $parallel->wait(true);
+        }catch (\Throwable $exception){
+            $e = $exception;
+        }
+
+        $this->expectExceptionObject($e);
+    }
+
+    /**
+     * @Synchronized(mode=LockMode::EXCEPTION)
+     */
+    public function handler():void{
+
+    }
 }
